87b74ed8bc0250e3db61666a9fd7a1d57a8e0afd
aksee <aksel.mahe@student.junia.com>
2022-02-23 10:03:35 +0100

    Ajout des spinners pour le panier, qu'on puisse choisir les sauces ou viande en fonction du plat qu'on veut

--- app/src/main/java/com/example/mi_order/fragments/CartFragments.kt
+++ app/src/main/java/com/example/mi_order/fragments/CartFragments.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
@@ -23,11 +24,43 @@ class CartFragment(
     ): View? {
         val view = inflater?.inflate(R.layout.fragment_cart, container, false)
 
+        //Déclaration des différents spinner pour alléger la fonction onItemSelected
+        val spinnerFood = view.findViewById<Spinner>(R.id.food_spinner)
+        val spinnerHotDog =  view.findViewById<Spinner>(R.id.hotdog_spinner)
+        val spinnerRamen = view.findViewById<Spinner>(R.id.ramen_spinner)
+        val spinnerCroque = view.findViewById<Spinner>(R.id.croque_spinner)
+        val spinnerPanini = view.findViewById<Spinner>(R.id.panini_spinner)
+        val spinnerSandwich = view.findViewById<Spinner>(R.id.sandwich_spinner)
+        //Afficher les spinners
+
+        spinnerFood.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                if (position == 0) spinnerHotDog.setVisibility(View.VISIBLE) else spinnerHotDog.setVisibility(
+                    View.GONE
+                )
+                if (position == 1) spinnerRamen.setVisibility(View.VISIBLE) else spinnerRamen.setVisibility(
+                    View.GONE
+                )
+                if (position == 2) spinnerCroque.setVisibility(View.VISIBLE) else spinnerCroque.setVisibility(
+                    View.GONE
+                )
+                if (position == 3) spinnerPanini.setVisibility(View.VISIBLE) else spinnerPanini.setVisibility(
+                    View.GONE
+                )
+                if (position == 4) spinnerSandwich.setVisibility(View.VISIBLE) else spinnerSandwich.setVisibility(
+                    View.GONE
+                )
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
         //Recuperer le bouton d'ajout
         val addButton = view.findViewById<Button>(R.id.confirm_button)
         addButton.setOnClickListener { sendOrder(view)}
         return view
     }
+
 
     private fun sendOrder(view: View) {
          val userName = view.findViewById<EditText>(R.id.name_input).text.toString()
